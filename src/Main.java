2ed53980e25ace9c131691b521f9544fe96403cc
soymze <soylmze@gmail.com>
2024-02-17 12:19:19 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+                int size;
+                System.out.println("Dizi boyutunu giriniz:");
+
+                Scanner sc = new Scanner(System.in);
+                size = sc.nextInt();
+
+                int[] arr1 = new int[size];
+
+                for(int i=0; i < arr1.length; i++){
+                    System.out.println("Dizinin "+(i+1)+". elemanını giriniz:");
+                    arr1[i] = sc.nextInt();
+                }
+                System.out.print("Sıralama: ");
+                Arrays.sort(arr1);
+                for(int i=0; i < arr1.length; i++){
+                    System.out.print(arr1[i]+"  ");
+                }
     }
 }
